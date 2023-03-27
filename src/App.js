b65fb9b55f185395e1339e342c75e2a2e5f25c4e
Abdus Sattar <sasorganicagro@gmail.com>
2023-03-28 03:30:22 +0600

    Add three components for calculator

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
 import React from 'react';
+import Calculator from './components/Calculator';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header" />
-    </div>
+    <Calculator />
   );
 }
 
