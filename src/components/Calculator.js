a265236e509d998b76d94f83de0cc50836058282
Abdus Sattar <sasorganicagro@gmail.com>
2023-03-29 01:56:50 +0600

    Add functionalities to modify all components

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,13 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
+
 import '../index.css';
 import Display from './Display';
 import Keypad from './Keypad';
+import calculate from '../logic/calculate';
 
-const Calculator = () => (
-  <section className="container">
-    <Display />
-    <Keypad />
-  </section>
-);
+const Calculator = () => {
+  const [displyValue, setDisplayValue] = useState(0);
+
+  const handleCalculation = (event) => {
+    setDisplayValue((displyValue) => calculate(displyValue, event.target.innerText));
+  };
+
+  return (
+    <section className="container">
+      <Display displyValue={displyValue.next || displyValue.total || '0'} />
+      <Keypad eventListener={handleCalculation} />
+    </section>
+  );
+};
 
 export default Calculator;
--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,25 +1,35 @@
-const Keypad = () => (
-  <div className="keypad">
-    <button type="button">AC</button>
-    <button type="button">+/-</button>
-    <button type="button">%</button>
-    <button type="button" className="deepOrange">&divide;</button>
-    <button type="button">7</button>
-    <button type="button">8</button>
-    <button type="button">9</button>
-    <button type="button" className="deepOrange">&times;</button>
-    <button type="button">4</button>
-    <button type="button">5</button>
-    <button type="button">6</button>
-    <button type="button" className="deepOrange">-</button>
-    <button type="button">1</button>
-    <button type="button">2</button>
-    <button type="button">3</button>
-    <button type="button" className="deepOrange">+</button>
-    <button type="button" className="zero">0</button>
-    <button type="button">.</button>
-    <button type="button" className="deepOrange">=</button>
-  </div>
-);
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const Keypad = (props) => {
+  const { eventListener } = props;
+  return (
+    <div className="keypad">
+      <button type="button" onClick={eventListener}>AC</button>
+      <button type="button" onClick={eventListener}>+/-</button>
+      <button type="button" onClick={eventListener}>%</button>
+      <button type="button" onClick={eventListener} className="deepOrange">&divide;</button>
+      <button type="button" onClick={eventListener}>7</button>
+      <button type="button" onClick={eventListener}>8</button>
+      <button type="button" onClick={eventListener}>9</button>
+      <button type="button" onClick={eventListener} className="deepOrange">x</button>
+      <button type="button" onClick={eventListener}>4</button>
+      <button type="button" onClick={eventListener}>5</button>
+      <button type="button" onClick={eventListener}>6</button>
+      <button type="button" onClick={eventListener} className="deepOrange">-</button>
+      <button type="button" onClick={eventListener}>1</button>
+      <button type="button" onClick={eventListener}>2</button>
+      <button type="button" onClick={eventListener}>3</button>
+      <button type="button" onClick={eventListener} className="deepOrange">+</button>
+      <button type="button" onClick={eventListener} className="zero">0</button>
+      <button type="button" onClick={eventListener}>.</button>
+      <button type="button" onClick={eventListener} className="deepOrange">=</button>
+    </div>
+  );
+};
+
+Keypad.propTypes = {
+  eventListener: PropTypes.func.isRequired,
+};
 
 export default Keypad;
